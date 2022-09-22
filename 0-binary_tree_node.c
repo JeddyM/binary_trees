23953ef9dfb1eb8bcefa177e086c069e5083631b
Jeddy Mweru <jeddymweru@gmail.com>
2022-09-22 21:04:20 +0000

    Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t new_node;
+	binary_tree_t *new_node;
 
 	if (new_node == NULL)
 		return (NULL);
@@ -17,8 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
-	new_node->right = Null;
-	new_node->left = Null;
+	new_node->right = NULL;
+	new_node->left = NULL;
 
 	return (new_node);
 }
