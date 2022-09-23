078a7b3c66ec4ae57cb4e20c4453443885a90ec0
Jeddy Mweru <jeddymweru@gmail.com>
2022-09-23 23:17:46 +0000

    task 5 and 4 edits

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,10 +7,15 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+
 	if (node->right == NULL && node->left == NULL)
 	{
 		return (1);
 
+	}
+	if (node == NULL)
+	{
+		return 0
 	}
 	return (0);
 }
